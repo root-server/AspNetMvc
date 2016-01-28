426a52a205320ee6066c9191c2672f3670ae48a9
Alex Ovcharov <hatekriss@gmail.com>
2016-01-28 23:36:21 +0300

    lesson 1. Start project. Install NLog and configure Log2Console

---
+++ AspNetMvc/AspNetMvc/App_Start/PreStartApp.cs
@@ -0,0 +1,19 @@
+﻿using AspNetMvc;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(PreStartApp), "Start")]
+namespace AspNetMvc
+{
+    public static class PreStartApp
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Метод запускается один раз перед стартом приложения        
+        /// </summary>
+        public static void Start()
+        {
+            logger.Info("Application PreStart");
+        }
+    }
+
+}
--- AspNetMvc/AspNetMvc/Global.asax.cs
+++ AspNetMvc/AspNetMvc/Global.asax.cs
@@ -10,12 +10,40 @@ namespace AspNetMvc
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
+            logger.Info("Application Start");
+
             AreaRegistration.RegisterAllAreas();
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public override void Init()
+        {
+            logger.Info("Application Init");
+            base.Init();
+        }
+
+        public override void Dispose()
+        {
+            logger.Info("Application Dispose");
+            base.Dispose();
+        }
+
+        protected void Application_Error()
+        {
+            logger.Info("Application Error");
+        }
+
+
+        protected void Application_End()
+        {
+            logger.Info("Application End");
         }
     }
 }
